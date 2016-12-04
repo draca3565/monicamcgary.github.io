1bf49809cad76eda350cd5c53705abc562d312e7
draca3565 <troyknife@gmail.com>
2016-12-03 23:02:19 -0700

    Weather

---
+++ Homework/javascript/wsearch.js
@@ -0,0 +1,35 @@
+
+$('#query').keyup(function(){
+    var value = $('#query').val();
+    var rExp = new RegExp(value, "i");
+
+    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+        console.log(data);
+        $('#query').keyup(function() {
+            var value = $('#query').val();
+            var rExp = new RegExp(value, "i");
+            if(value == ""){
+                $("#searchResults").html("");
+            } else{
+                $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
+                    console.log(data); // test for JSON received
+                    // Begin building output
+                    var output = '<ol>';
+                    $.each(data.RESULTS, function(key, val) {
+                        if (val.name.search(rExp) != -1) {
+                            output += '<li>';
+                            output += '<a href="#" onclick="getData(' + val.lat + ','+val.lon+')" title="See results for ' + val.name + '">' + val.name + '</a>';
+                            output += '</li>';
+                        }
+                    }); // end each
+                    output += '</ol>';
+                    $("#searchResults").html(output); // send results to the page
+
+                }); // end getJSON
+            } // end if there are characters in a query
+
+        }); // end onkeyup
+
+    }); // end getJSON
+
+}); // end keyup
